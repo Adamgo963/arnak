333a8515b55ad9dbc258955a377cca51e8bdacb5
Adamgo <adamgo963@gmail.com>
2025-01-16 16:49:34 +0100

    initial commit

--- arnak-be/server.js
+++ arnak-be/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
+const staticPath = path.resolve(__dirname, ".", "dist");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
@@ -21,5 +23,13 @@ io.on("connection", (socket) => {
     console.log("Client disconnected.");
   });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    app.use(express.static(staticPath));
+    const indexFile = path.join(__dirname, "dist", "index.html");
+    return res.sendFile(indexFile);
+  });
+}
 
 server.listen(5000, () => console.log("Server listening on port 5000."));
